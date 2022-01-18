67f4318a381811f8198751fcec2256569588bca0
Ciaran Hogan <gm.ciaran.hogan@gmail.com>
2022-01-17 21:34:50 -0700

    Renamed example files.

--- PlayC4Example1.m
+++ PlayC4Example1.m
@@ -3,7 +3,7 @@ clc
 
 connect = Connect4Board.Empty();
 outcomes = GameOutcomes();
-    
+
 while (connect.IsGameOver(connect.CurrentPlayer()) == 0)
     
     move = AI.GetMoveNegamax(connect,8,outcomes);
--- Untitled.m
+++
@@ -1,9 +0,0 @@
-clear
-clc
-
-board = TTTBoard.Empty();
-
-move = AI.GetMoveNegamax(board,9,GameOutcomes());
-board = board.MakeMove(move);
-
-board.PrintBoard();
